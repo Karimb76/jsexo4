8405c01c3ba059f93dfadf8aebe0419156fc5dfd
karimb76 <karim.bellet@gmail.com>
2018-10-08 15:11:51 +0200

    maj

---
+++ js.js
@@ -0,0 +1,9 @@
+ // functionName
+
+}
+function myFunction(){
+var firstName1=   document.forms["form"]["name"].value;
+var lastName1=  document.forms["form"]["firstname"].value;
+var city1=  document.forms["form"]["city"].value;
+alert("Nom :"+firstName1+"\nPrénom :"+lastName1+"\nVille :"+city1);
+}
